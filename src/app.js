009f2401e527c576e593d2b6070e76525e1a4f26
mohankumar-markuli <mohankumarmarkuli@gmail.com>
2026-05-03 18:25:03 +0530

    feat: initialize project structure with environment configuration, middleware, and server setup

---
+++ src/app.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+const cookieParser = require("cookie-parser");
+
+const { logger } = require('./middlewares/logger');
+const { errorHandler } = require("./middlewares/errorHandler");
+
+// DNS
+const dns = require("dns");
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
+
+app.use(logger);
+app.use(express.json());
+app.use(cookieParser());
+
+// routes
+// const authRouter = require('./routes/authRoutes');
+// const userRouter = require('./routes/userRoutes');
+
+
+// app.use("/api/v1/auth", authRouter);
+// app.use("/api/v1/users", userRouter);
+
+
+// health
+app.get("/api/v1/health", (req, res) => {
+    res.json({
+        status: "OK",
+        message: "Server is running",
+        timestamp: new Date().toISOString()
+    });
+});
+
+// error handler
+app.use(errorHandler);
+
+module.exports = app;
