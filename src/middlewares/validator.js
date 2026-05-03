2780f3f2ee44dea5491325aa72187b3373c9490b
mohankumar-markuli <mohankumarmarkuli@gmail.com>
2026-05-03 23:33:16 +0530

    feat: add validation middleware for edit user input data

--- src/middlewares/validator.js
+++ src/middlewares/validator.js
@@ -62,8 +62,41 @@ const validateChangePassword = async (req, res, next) => {
     }
 }
 
+const validateFields = (keys, allowed, restricted) => {
+    const forbidden = keys.filter(k => restricted.has(k));
+    if (forbidden.length) {
+        throw new Error(`Restricted fields: ${forbidden.join(", ")}`);
+    }
+
+    const invalid = keys.filter(k => !allowed.has(k));
+    if (invalid.length) {
+        throw new Error(`Invalid fields: ${invalid.join(", ")}`);
+    }
+
+};
+
+const validateEditUserData = (req, res, next) => {
+    try {
+        const ALLOWED_FIELDS = new Set(["firstName", "lastName"]);
+        const RESTRICTED_FIELDS = new Set(["emailId", "password"]);
+
+        const keys = Object.keys(req.body || {});
+
+        if (keys.length === 0) {
+            throw new Error("No fields provided for update");
+        }
+        validateFields(keys, ALLOWED_FIELDS, RESTRICTED_FIELDS);
+
+        next();
+
+    } catch (err) {
+        next(err);
+    }
+};
+
 module.exports = {
     validateSignUpData,
     validatePassword,
-    validateChangePassword
+    validateChangePassword,
+    validateEditUserData
 };
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,12 +3,12 @@ const userRouter = express.Router();
 
 const { userAuth } = require("../middlewares/userAuth");
 const { viewUser, editUser, changePassword } = require("../controllers/userController");
-const { validateChangePassword } = require("../middlewares/validator");
+const { validateEditUserData, validateChangePassword } = require("../middlewares/validator");
 
 userRouter.use(userAuth);
 
 userRouter.get('/profile', viewUser);
-userRouter.patch('/profile', editUser);
+userRouter.patch('/profile', validateEditUserData, editUser);
 userRouter.patch('/password', validateChangePassword, changePassword);
 
 module.exports = userRouter;
