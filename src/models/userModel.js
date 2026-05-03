f1de7e10e4f2b55f3f103f4c7218f2b0bc54669e
mohankumar-markuli <mohankumarmarkuli@gmail.com>
2026-05-03 23:18:09 +0530

    fix: set default value for user preferences languages to "en"

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -43,7 +43,7 @@ const userSchema = new mongoose.Schema({
         languages: {
             type: [String],
             enum: LANGUAGE_ENUM,
-            default: []
+            default: ["en"]
         },
         countries: {
             type: String,
