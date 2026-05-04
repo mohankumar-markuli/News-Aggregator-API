116242c2f812f7e37523f55dfac5e5ed49ff1969
mohankumar-markuli <mohankumarmarkuli@gmail.com>
2026-05-04 19:55:16 +0530

    fix: comment out console log in error handler to reduce noise in logs

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,5 +1,5 @@
 const errorHandler = (err, req, res, next) => {
-    console.log(err);
+    // console.log(err);
     res.status(err.statusCode || err.status || 500).json({
         message: err.message || "Internal Server Error",
     });
